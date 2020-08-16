59f843fab8e0d4dff2c22a93041fd36b4e12b820
okue <nogideca@gmail.com>
2020-08-16 22:42:10 +0900

    update

--- grpc-kotlin-armeria/build.gradle.kts
+++ grpc-kotlin-armeria/build.gradle.kts
@@ -13,4 +13,9 @@ dependencies {
 
 application {
     mainClassName = "example.kt.armeria.ArmeriaGrpcApplicationKt"
+    applicationDefaultJvmArgs = listOf(
+        "-server", "-XX:+UseG1GC",
+        "-Xlog:gc*=debug,age*=debug,ergo*=debug,safepoint,class+unload:/tmp/logs/jvm.log:time,uptime,level,tags:filesize=512m,filecount=10",
+        "-Xmx2g", "-Xms2g"
+    )
 }
--- grpc-kotlin/build.gradle.kts
+++ grpc-kotlin/build.gradle.kts
@@ -11,4 +11,9 @@ dependencies {
 
 application {
     mainClassName = "example.kt.GrpcApplicationKt"
+    applicationDefaultJvmArgs = listOf(
+        "-server", "-XX:+UseG1GC",
+        "-Xlog:gc*=debug,age*=debug,ergo*=debug,safepoint,class+unload:/tmp/logs/jvm.log:time,uptime,level,tags:filesize=512m,filecount=10",
+        "-Xmx2g", "-Xms2g"
+    )
 }
